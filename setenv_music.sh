2495f2dd27549a540bdc1f3f34dc861167a63899
Felipe Silva <felipe.silva@cern.ch>
2022-07-07 18:31:14 +0200

    DIR --> SCRIPTDIR

--- setenv_music.sh
+++ setenv_music.sh
@@ -1,49 +1,51 @@
 #!/bin/bash
 
 # This is a set_env script
-DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPTDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # load user configs
 source music_env.config
 
+echo $SCRIPTDIR
+
 
 # Set up the CMSSW environment
-cd $DIR/$CMSSW_VER/src/PxlSkimmer
+cd $SCRIPTDIR/$CMSSW_VER/src/PxlSkimmer
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 cmsenv
 source set_env.sh
 
 # This is a TAPAS set_env script. Source it before usage of TAPAS.
-export LD_LIBRARY_PATH=$DIR/tapas/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=$DIR/tapas/lib/python:$PYTHONPATH
+export LD_LIBRARY_PATH=$SCRIPTDIR/tapas/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=$SCRIPTDIR/tapas/lib/python:$PYTHONPATH
 export MYPXLANA=EventClassFactory
 
-
+echo $SCRIPTDIR
 # Set up CRAB3
 source /cvmfs/cms.cern.ch/crab3/crab.sh
-
+echo $SCRIPTDIR
 # Source all the needed set_env
-cd $DIR/tapas/tools/
+cd $SCRIPTDIR/tapas/tools/
 source set_env.sh
 
-cd $DIR/tapas/PxlAnalyzer/
+cd $SCRIPTDIR/tapas/PxlAnalyzer/
 source set_env.sh
 
-cd $DIR/tapas/PlotLib/
+cd $SCRIPTDIR/tapas/PlotLib/
 source set_env.sh
 
-cd $DIR/tapas/PxlAnalyzer/EventClassFactory/
+cd $SCRIPTDIR/tapas/PxlAnalyzer/EventClassFactory/
 source set_env.sh
 
-cd $DIR/tapas/MUSiC-Utils/
+cd $SCRIPTDIR/tapas/MUSiC-Utils/
 source set_env.sh
 
-cd $DIR/tapas/MUSiC-Configs/
+cd $SCRIPTDIR/tapas/MUSiC-Configs/
 source set_env.sh
 
-cd $DIR/tapas/MUSiC-RoIScanner/
+cd $SCRIPTDIR/tapas/MUSiC-RoIScanner/
 source set_env.sh
 
-cd $DIR
+cd $SCRIPTDIR
 
 echo "Initialize your grid certificate..."
 voms-proxy-init --voms cms:/cms --valid 192:0
