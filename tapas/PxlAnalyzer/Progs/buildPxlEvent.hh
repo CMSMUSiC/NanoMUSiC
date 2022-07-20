9e66748f20c176214c818eb3960fa876533b500e
Felipe Silva <felipe.silva@cern.ch>
2022-07-20 20:22:58 +0200

    add parser for nanoaod

--- tapas/PxlAnalyzer/Progs/buildPxlEvent.hh
+++ tapas/PxlAnalyzer/Progs/buildPxlEvent.hh
@@ -1,10 +1,14 @@
 #include<iostream>
 
-#include "Pxl/Pxl/interface/pxl/hep.hh"
+#include "TTree.h"
 #include "Pxl/Pxl/interface/pxl/core.hh"
 
-pxl::Event * buildPxlEvent() {
+// This function will read a NanoAOD event from a tree and return a pxl::Event
+pxl::Event * buildPxlEvent(int i_evt, TTree * tree_ptr, bool isData) {
   pxl::Event * event_ptr = new pxl::Event();
+  tree_ptr->GetEntry(i_evt);
+
+  // return the produced pxl:Event
   return event_ptr;
 } 
 
