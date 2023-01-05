7e2616859900e2ebf92ba1afdf98c2b3b661a9e5
Felipe Silva <felipe.silva@cern.ch>
2023-01-05 12:35:32 +0100

    Add a proper directory structure for thr Rust TTreeReader bindings.

--- NanoMUSiC/MUSiC-Classification/classification/build.rs
+++ NanoMUSiC/MUSiC-Classification/classification/build.rs
@@ -1,80 +1,13 @@
 use std::env;
-// use std::path::Path;
-// use std::process::Command;
-fn main() {
-    // cxx_build::bridge("src/main.rs")
-    //     .file("src/ttree_reader.cpp")
-    //     .flag_if_supported("-std=c++17")
-    //     .compile("ttree_reader");
 
+fn main() {
     println!("cargo:rerun-if-changed=src/ttree_reader.cpp");
     println!("cargo:rerun-if-changed=build.rs");
 
-    // let out_dir = env::var("OUT_DIR").unwrap();
     let cc_env = env::var("CC").unwrap();
-
-    // // compile
-    // Command::new("g++")
-    //     .args(&["-pthread", "-std=c++17", "-m64", "-I/cvmfs/sft.cern.ch/lcg/releases/ROOT/6.26.08-34ede/x86_64-centos7-gcc12-opt/include", "-L/cvmfs/sft.cern.ch/lcg/releases/ROOT/6.26.08-34ede/x86_64-centos7-gcc12-opt/lib", "-lCore", "-lImt", "-lRIO", "-lNet", "-lHist", "-lGraf", "-lGraf3d", "-lGpad", "-lROOTVecOps", "-lTree", "-lTreePlayer", "-lRint", "-lPostscript", "-lMatrix", "-lPhysics", "-lMathCore", "-lThread", "-lMultiProc", "-lROOTDataFrame", "-Wl,-rpath,/cvmfs/sft.cern.ch/lcg/releases/ROOT/6.26.08-34ede/x86_64-centos7-gcc12-opt/lib", "-lm", "-ldl", "-rdynamic", "src/ttree_reader.cpp", "-c", "-fPIC", "-o"])
-    //     .arg(&format!("{}/ttree_reader.o", out_dir))
-    //     .status()
-    //     .unwrap();
-
-    // compile
-    // Command::new("g++")
-    //     .args(&[
-    //         "-std=c++17",
-    //         "-m64",
-    //         "src/ttree_reader.cpp",
-    //         "-c",
-    //         "-fPIC",
-    //         "-Wall",
-    //         "-Wextra",
-    //         "-Werror",
-    //         "-o",
-    //         "-lstdc++",
-    //     ])
-    //     .arg(&format!("{}/ttree_reader.o", out_dir))
-    //     .status()
-    //     .unwrap();
-
-    // // make static
-    // Command::new("ar")
-    //     .args(&["crus", "libttreereader.a", "ttree_reader.o"])
-    //     .current_dir(&Path::new(&out_dir))
-    //     .status()
-    //     .unwrap();
-
-    // println!("cargo:rustc-link-search=native={}", out_dir);
-    // println!("cargo:rustc-link-search=native=/usr/lib64");
-    // println!("cargo:rustc-link-lib=stdc++");
-    // println!("cargo:rustc-link-search=native=/cvmfs/sft.cern.ch/lcg/releases/ROOT/6.26.08-34ede/x86_64-centos7-gcc12-opt/lib");
-    // println!("cargo:rustc-link-lib=dylib=Core");
-    // println!("cargo:rustc-link-lib=dylib=Imt");
-    // println!("cargo:rustc-link-lib=dylib=RIO");
-    // println!("cargo:rustc-link-lib=dylib=Net");
-    // println!("cargo:rustc-link-lib=dylib=Hist");
-    // println!("cargo:rustc-link-lib=dylib=Graf");
-    // println!("cargo:rustc-link-lib=dylib=Graf3d");
-    // println!("cargo:rustc-link-lib=dylib=Gpad");
-    // println!("cargo:rustc-link-lib=dylib=ROOTVecOps");
-    // println!("cargo:rustc-link-lib=dylib=Tree");
-    // println!("cargo:rustc-link-lib=dylib=TreePlayer");
-    // println!("cargo:rustc-link-lib=dylib=Rint");
-    // println!("cargo:rustc-link-lib=dylib=Postscript");
-    // println!("cargo:rustc-link-lib=dylib=Matrix");
-    // println!("cargo:rustc-link-lib=dylib=Physics");
-    // println!("cargo:rustc-link-lib=dylib=MathCore");
-    // println!("cargo:rustc-link-lib=dylib=Thread");
-    // println!("cargo:rustc-link-lib=dylib=MultiProc");
-    // println!("cargo:rustc-link-lib=dylib=ROOTDataFrame");
-    // println!("cargo:rustc-link-lib=static=ttreereader");
-    // println!("cargo:rerun-if-changed=src/ttree_reader.cpp");
-    // println!("cargo:rerun-if-changed=build.rs");
-
     cc::Build::new()
         .cpp(true)
-        .file("src/ttree_reader.cpp")
+        .file("src/tree_reader.cpp")
         .extra_warnings(true)
         .cpp_link_stdlib("stdc++")
         .warnings(true)
--- NanoMUSiC/MUSiC-Classification/classification/src/main.rs
+++ NanoMUSiC/MUSiC-Classification/classification/src/main.rs
@@ -1,20 +1,112 @@
-use libc::c_void;
+// use libc;
+use std::ffi::CString;
+use std::os::raw::c_char;
 
-fn bar(oi: i32) -> i32 {
-    oi
+use root_binds::rvec_float;
+use std::slice;
+
+mod root_binds {
+
+    #[repr(C)]
+    pub struct TTreeReader {
+        _private: [u8; 0],
+    }
+
+    #[repr(C)]
+    pub struct TTreeReaderValueFloat {
+        _private: [u8; 0],
+    }
+
+    #[repr(C)]
+    pub struct ValueReaders {
+        _private: [u8; 0],
+        pub px: *mut TTreeReaderValueFloat,
+        pub py: *mut TTreeReaderValueFloat,
+    }
+
+    #[repr(C)]
+    pub struct rvec_float {
+        pub data: *const libc::c_float,
+        pub len: libc::size_t,
+    }
+
+    #[repr(C)]
+    pub struct vecrvec_float {
+        pub data: *const libc::c_void,
+        pub len: libc::size_t,
+    }
+
+    // #[repr(C)]
+    // pub struct vecvec_float {
+    //     _private: [u8; 0],
+    //     pub data: *mut rvec_float,
+    //     pub len: libc::size_t,
+    // }
 }
 
 extern "C" {
-    fn get_file() -> c_void;
-    fn ttree_reader(file: c_void);
+
+    fn get_float_arrays(
+        tree_name: *const c_char,
+        file_path: *const c_char,
+    ) -> *mut root_binds::vecrvec_float;
+    // ) -> root_binds::rvec_float;
+
+    fn get_tree_reader(
+        tree_name: *const c_char,
+        file_path: *const c_char,
+    ) -> *mut root_binds::TTreeReader;
+
+    fn get_value_readers(
+        tree_reader: *mut root_binds::TTreeReader,
+    ) -> *mut root_binds::ValueReaders;
+
+    fn next(tree_reader: *mut root_binds::TTreeReader) -> bool;
+
+    fn get_value(value_reader: *mut root_binds::TTreeReaderValueFloat) -> f32;
+
+    fn process_tree(tree_reader: *mut root_binds::TTreeReader);
 }
 
 fn main() {
-    let foo = 32;
-    println!("Hello, world - {} !", bar(foo));
-    // ffi::ttree_reader();
+    let tree_name = CString::new("ntuple").unwrap();
+    let file_path = CString::new("$ROOTSYS/tutorials/hsimple.root").unwrap();
 
     unsafe {
-        ttree_reader(get_file());
+        let tree_reader = get_tree_reader(tree_name.as_ptr(), file_path.as_ptr());
+        let value_readers = get_value_readers(tree_reader);
+
+        let mut event_count: i32 = 0;
+        let mut sum: f32 = 0.;
+        while next(tree_reader) {
+            event_count += 1;
+            sum += get_value((*value_readers).px) + get_value((*value_readers).py);
+        }
+        println!("Sum: {}", sum);
+        println!("Event count: {}", event_count);
+
+        process_tree(get_tree_reader(tree_name.as_ptr(), file_path.as_ptr()));
+    }
+
+    // RDataFrame ...
+    let tree_name = CString::new("Events").unwrap();
+    let file_path = CString::new("/home/home1/institut_3a/silva/projects/NanoMUSiC_Test_Data/DY_2017/04426F83-BF53-0440-B2D1-F2DD9AB96EB8.root").unwrap();
+
+    let float_arrays_of_array =
+        unsafe { &*get_float_arrays(tree_name.as_ptr(), file_path.as_ptr()) };
+
+    unsafe {
+        println!("Float Arrays: {:?}", float_arrays_of_array.len);
+    }
+
+    for idx in 0..float_arrays_of_array.len {
+        //     // println!("Event: {}", evt);
+        //     let float_arrays_slice =
+        let evt = root_binds::rvec_float {
+            data: (float_arrays_of_array + idx).data,
+            len: (float_arrays_of_array + idx).len,
+        };
+        let float_arrays = unsafe { slice::from_raw_parts(evt.data as f32, evt.len) };
+        println!("Float Arrays: {:?}", float_arrays);
     }
 }
