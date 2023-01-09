2bb3fad7ec2a27e587b8ace8417a7b752c7c3bde
Felipe Silva <felipe.silva@cern.ch>
2023-01-09 14:11:24 +0100

    Outputs are composed of only simple arrays.

--- NanoMUSiC/MUSiC/include/Outputs.hpp
+++ NanoMUSiC/MUSiC/include/Outputs.hpp
@@ -36,9 +36,7 @@ namespace IndexHelpers
 {
 
 ////////////////////////////////////////////////////////////////////////////////
-/// Create a 1-Dim histogram with variable bins of type float
-///
-/// (see TH1::TH1 for explanation of parameters)
+/// Creates a iterable range of integers in the interval [first, last].
 const std::vector<long> make_index(long first, long last)
 {
     auto vec = std::vector<long>(last - first + 1);
@@ -51,6 +49,8 @@ const std::vector<long> make_index(long first, long last)
     return vec;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// Creates a iterable range of integers in the interval [0 (zero), length[.
 const std::vector<long> make_index(long lenght)
 {
     return make_index(0, lenght - 1);
@@ -73,9 +73,11 @@ class Outputs
     static constexpr auto Shifts = make_enumerate("Nominal", "Up", "Down");
 
     static constexpr auto kTotalCuts = Outputs::Cuts.size();
-    static constexpr auto kTotalWeights = Outputs::Weights.size();
+    static constexpr unsigned int kTotalWeights = Outputs::Weights.size();
     // static constexpr auto kTotalVariations = Outputs::Variations.size();
     static constexpr auto kTotalshifts = Outputs::Shifts.size();
+
+    static constexpr unsigned int kMaxObjects = 100;
 
     // output file
     std::unique_ptr<TFile> output_file;
@@ -89,43 +91,45 @@ class Outputs
     unsigned long event_number;
     unsigned long trigger_bits;
 
+    // ROOT won't let us write a const to a TTree 🤔🤬 ...
+    int kTotalWeights_non_const = kTotalWeights;
     std::array<float, kTotalWeights> weights_nominal;
     std::array<float, kTotalWeights> weights_up;
     std::array<float, kTotalWeights> weights_down;
 
     unsigned int nMuon;
-    RVec<float> Muon_pt;
-    RVec<float> Muon_eta;
-    RVec<float> Muon_phi;
+    std::array<float, kMaxObjects> Muon_pt;
+    std::array<float, kMaxObjects> Muon_eta;
+    std::array<float, kMaxObjects> Muon_phi;
 
     unsigned int nElectron;
-    RVec<float> Electron_pt;
-    RVec<float> Electron_eta;
-    RVec<float> Electron_phi;
+    std::array<float, kMaxObjects> Electron_pt;
+    std::array<float, kMaxObjects> Electron_eta;
+    std::array<float, kMaxObjects> Electron_phi;
 
     unsigned int nPhoton;
-    RVec<float> Photon_pt;
-    RVec<float> Photon_eta;
-    RVec<float> Photon_phi;
+    std::array<float, kMaxObjects> Photon_pt;
+    std::array<float, kMaxObjects> Photon_eta;
+    std::array<float, kMaxObjects> Photon_phi;
 
     unsigned int nTau;
-    RVec<float> Tau_pt;
-    RVec<float> Tau_eta;
-    RVec<float> Tau_phi;
+    std::array<float, kMaxObjects> Tau_pt;
+    std::array<float, kMaxObjects> Tau_eta;
+    std::array<float, kMaxObjects> Tau_phi;
 
     unsigned int nBJet;
-    RVec<float> BJet_pt;
-    RVec<float> BJet_eta;
-    RVec<float> BJet_phi;
+    std::array<float, kMaxObjects> BJet_pt;
+    std::array<float, kMaxObjects> BJet_eta;
+    std::array<float, kMaxObjects> BJet_phi;
 
     unsigned int nJet;
-    RVec<float> Jet_pt;
-    RVec<float> Jet_eta;
-    RVec<float> Jet_phi;
+    std::array<float, kMaxObjects> Jet_pt;
+    std::array<float, kMaxObjects> Jet_eta;
+    std::array<float, kMaxObjects> Jet_phi;
 
     unsigned int nMET;
-    RVec<float> MET_pt;
-    RVec<float> MET_phi;
+    std::array<float, kMaxObjects> MET_pt;
+    std::array<float, kMaxObjects> MET_phi;
 
     // cutflow
     TH1F cutflow_histo;
@@ -138,40 +142,49 @@ class Outputs
         output_tree->SetDirectory(output_file.get());
 
         // tree branches
-        output_tree->Branch("run", &run);
-        output_tree->Branch("lumi_section", &lumi_section);
-        output_tree->Branch("event_number", &event_number);
-        output_tree->Branch("trigger_bits", &trigger_bits);
-        output_tree->Branch("weights_nominal", &weights_nominal);
-        output_tree->Branch("weights_up", &weights_up);
-        output_tree->Branch("weights_down", &weights_down);
-        output_tree->Branch("nMuon", &nMuon);
-        output_tree->Branch("Muon_pt", &Muon_pt);
-        output_tree->Branch("Muon_eta", &Muon_eta);
-        output_tree->Branch("Muon_phi", &Muon_phi);
-        output_tree->Branch("nElectron", &nElectron);
-        output_tree->Branch("Electron_pt", &Electron_pt);
-        output_tree->Branch("Electron_eta", &Electron_eta);
-        output_tree->Branch("Electron_phi", &Electron_phi);
-        output_tree->Branch("nPhoton", &nPhoton);
-        output_tree->Branch("Photon_pt", &Photon_pt);
-        output_tree->Branch("Photon_eta", &Photon_eta);
-        output_tree->Branch("Photon_phi", &Photon_phi);
-        output_tree->Branch("nTau", &nTau);
-        output_tree->Branch("Tau_pt", &Tau_pt);
-        output_tree->Branch("Tau_eta", &Tau_eta);
-        output_tree->Branch("Tau_phi", &Tau_phi);
-        output_tree->Branch("nBJet", &nBJet);
-        output_tree->Branch("BJet_pt", &BJet_pt);
-        output_tree->Branch("BJet_eta", &BJet_eta);
-        output_tree->Branch("BJet_phi", &BJet_phi);
-        output_tree->Branch("nJet", &nJet);
-        output_tree->Branch("Jet_pt", &Jet_pt);
-        output_tree->Branch("Jet_eta", &Jet_eta);
-        output_tree->Branch("Jet_phi", &Jet_phi);
-        output_tree->Branch("nMET", &nMET);
-        output_tree->Branch("MET_pt", &MET_pt);
-        output_tree->Branch("MET_phi", &MET_phi);
+        // Reference: https://root.cern.ch/doc/v626/classTTree.html#a1b2944a5a65655e135d102883a96daf2
+        // meta and counters
+        output_tree->Branch("run", &run, "run/i");
+        output_tree->Branch("lumi_section", &lumi_section, "lumi_section/i");
+        output_tree->Branch("event_number", &event_number, "event_number/g");
+        output_tree->Branch("trigger_bits", &trigger_bits, "trigger_bits/g");
+
+        output_tree->Branch("kTotalWeights", &kTotalWeights_non_const, "kTotalWeights/i");
+
+        output_tree->Branch("nMuon", &nMuon, "nMuon/i");
+        output_tree->Branch("nElectron", &nElectron, "nElectron/i");
+        output_tree->Branch("nPhoton", &nPhoton, "nPhoton/i");
+        output_tree->Branch("nTau", &nTau, "nTau/i");
+        output_tree->Branch("nBJet", &nBJet, "nBJet/i");
+        output_tree->Branch("nJet", &nJet, "nJet/i");
+        output_tree->Branch("nMET", &nMET, "nMET/i");
+
+        // weights
+        output_tree->Branch("weights_nominal", weights_nominal.data(), "weights_nominal[kTotalWeights]/F");
+        // output_tree->Branch("weights_up", weights_up.data(), "weights_up[kTotalWeights]/F");
+        // output_tree->Branch("weights_down", weights_down.data(), "weights_down[kTotalWeights]/F");
+
+        // // physical objects
+        // output_tree->Branch("Muon_pt", Muon_pt.data(), "Muon_pt[nMuon]/F");
+        // output_tree->Branch("Muon_eta", Muon_eta.data(), "Muon_eta[nMuon]/F");
+        // output_tree->Branch("Muon_phi", Muon_phi.data(), "Muon_phi[nMuon]/F");
+        // output_tree->Branch("Electron_pt", Electron_pt.data(), "Electron_pt[nElectron]/F");
+        // output_tree->Branch("Electron_eta", Electron_eta.data(), "Electron_eta[nElectron]/F");
+        // output_tree->Branch("Electron_phi", Electron_phi.data(), "Electron_phi[nElectron]/F");
+        // output_tree->Branch("Photon_pt", Photon_pt.data(), "Photon_pt[nPhoton]/F");
+        // output_tree->Branch("Photon_eta", Photon_eta.data(), "Photon_eta[nPhoton]/F");
+        // output_tree->Branch("Photon_phi", Photon_phi.data(), "Photon_phi[nPhoton]/F");
+        // output_tree->Branch("Tau_pt", Tau_pt.data(), "Tau_pt[nTau]/F");
+        // output_tree->Branch("Tau_eta", Tau_eta.data(), "Tau_eta[nTau]/F");
+        // output_tree->Branch("Tau_phi", Tau_phi.data(), "Tau_phi[nTau]/F");
+        // output_tree->Branch("BJet_pt", BJet_pt.data(), "BJet_pt[nBJet]/F");
+        // output_tree->Branch("BJet_eta", BJet_eta.data(), "BJet_eta[nBJet]/F");
+        // output_tree->Branch("BJet_phi", BJet_phi.data(), "BJet_phi[nBJet]/F");
+        // output_tree->Branch("Jet_pt", Jet_pt.data(), "Jet_pt[nJet]/F");
+        // output_tree->Branch("Jet_eta", Jet_eta.data(), "Jet_eta[nJet]/F");
+        // output_tree->Branch("Jet_phi", Jet_phi.data(), "Jet_phi[nJet]/F");
+        // output_tree->Branch("MET_pt", MET_pt.data(), "MET_pt[nMET]/F");
+        // output_tree->Branch("MET_phi", MET_phi.data(), "MET_phi[nMET]/F");
 
         // make cutflow histos
         const std::string histo_name = "cutflow";
@@ -231,46 +244,86 @@ class Outputs
         event_number = 0;
         trigger_bits = 0;
 
-        for (auto &&idx_weight : Outputs::Weights)
-        {
-            weights_nominal.at(Outputs::Weights.index_of(idx_weight)) = 1.;
-            weights_up.at(Outputs::Weights.index_of(idx_weight)) = 1.;
-            weights_down.at(Outputs::Weights.index_of(idx_weight)) = 1.;
-        }
+        weights_nominal.fill(1);
+        weights_up.fill(1);
+        weights_down.fill(1);
 
         nMuon = 0;
-        Muon_pt.clear();
-        Muon_eta.clear();
-        Muon_phi.clear();
+        Muon_pt.fill(0);
+        Muon_eta.fill(0);
+        Muon_phi.fill(0);
 
         nElectron = 0;
-        Electron_pt.clear();
-        Electron_eta.clear();
-        Electron_phi.clear();
+        Electron_pt.fill(0);
+        Electron_eta.fill(0);
+        Electron_phi.fill(0);
 
         nPhoton = 0;
-        Photon_pt.clear();
-        Photon_eta.clear();
-        Photon_phi.clear();
+        Photon_pt.fill(0);
+        Photon_eta.fill(0);
+        Photon_phi.fill(0);
 
         nTau = 0;
-        Tau_pt.clear();
-        Tau_eta.clear();
-        Tau_phi.clear();
+        Tau_pt.fill(0);
+        Tau_eta.fill(0);
+        Tau_phi.fill(0);
 
         nBJet = 0;
-        BJet_pt.clear();
-        BJet_eta.clear();
-        BJet_phi.clear();
+        BJet_pt.fill(0);
+        BJet_eta.fill(0);
+        BJet_phi.fill(0);
 
         nJet = 0;
-        Jet_pt.clear();
-        Jet_eta.clear();
-        Jet_phi.clear();
+        Jet_pt.fill(0);
+        Jet_eta.fill(0);
+        Jet_phi.fill(0);
 
         nMET = 0;
-        MET_pt.clear();
-        MET_phi.clear();
+        MET_pt.fill(0);
+        MET_phi.fill(0);
+    }
+
+    void fill_branches(RVec<float> &&_muon_pt, RVec<float> &&_muon_eta, RVec<float> &&_muon_phi, RVec<float> &&_electron_pt,
+                       RVec<float> &&_electron_eta, RVec<float> &&_electron_phi, RVec<float> &&_photon_pt,
+                       RVec<float> &&_photon_eta, RVec<float> &&_photon_phi, RVec<float> &&_tau_pt, RVec<float> &&_tau_eta,
+                       RVec<float> &&_tau_phi, RVec<float> &&_bjet_pt, RVec<float> &&_bjet_eta, RVec<float> &&_bjet_phi,
+                       RVec<float> &&_jet_pt, RVec<float> &&_jet_eta, RVec<float> &&_jet_phi, RVec<float> &&_met_pt,
+                       RVec<float> &&_met_phi)
+    {
+
+        nMuon = _muon_pt.size();
+        std::copy(_muon_pt.cbegin(), _muon_pt.cend(), Muon_pt.begin());
+        std::copy(_muon_eta.cbegin(), _muon_eta.cend(), Muon_eta.begin());
+        std::copy(_muon_phi.cbegin(), _muon_phi.cend(), Muon_phi.begin());
+
+        nElectron = _electron_pt.size();
+        std::copy(_electron_pt.cbegin(), _electron_pt.cend(), Electron_pt.begin());
+        std::copy(_electron_eta.cbegin(), _electron_eta.cend(), Electron_eta.begin());
+        std::copy(_electron_phi.cbegin(), _electron_phi.cend(), Electron_phi.begin());
+
+        nPhoton = _photon_pt.size();
+        std::copy(_photon_pt.cbegin(), _photon_pt.cend(), Photon_pt.begin());
+        std::copy(_photon_eta.cbegin(), _photon_eta.cend(), Photon_eta.begin());
+        std::copy(_photon_phi.cbegin(), _photon_phi.cend(), Photon_phi.begin());
+
+        nTau = _tau_pt.size();
+        std::copy(_tau_pt.cbegin(), _tau_pt.cend(), Tau_pt.begin());
+        std::copy(_tau_eta.cbegin(), _tau_eta.cend(), Tau_eta.begin());
+        std::copy(_tau_phi.cbegin(), _tau_phi.cend(), Tau_phi.begin());
+
+        nBJet = _bjet_pt.size();
+        std::copy(_bjet_pt.cbegin(), _bjet_pt.cend(), BJet_pt.begin());
+        std::copy(_bjet_eta.cbegin(), _bjet_eta.cend(), BJet_eta.begin());
+        std::copy(_bjet_phi.cbegin(), _bjet_phi.cend(), BJet_phi.begin());
+
+        nJet = _jet_pt.size();
+        std::copy(_jet_pt.cbegin(), _jet_pt.cend(), Jet_pt.begin());
+        std::copy(_jet_eta.cbegin(), _jet_eta.cend(), Jet_eta.begin());
+        std::copy(_jet_phi.cbegin(), _jet_phi.cend(), Jet_phi.begin());
+
+        nMET = _met_pt.size();
+        std::copy(_met_pt.cbegin(), _met_pt.cend(), MET_pt.begin());
+        std::copy(_met_phi.cbegin(), _met_phi.cend(), MET_phi.begin());
     }
 
     void fill_event_tree()
@@ -282,48 +335,6 @@ class Outputs
     void fill_cutflow_histo(std::string_view cut, float weight)
     {
         cutflow_histo.Fill(Outputs::Cuts.index_of(cut), weight);
-    }
-
-    void fill_branches(RVec<float> &&_muon_pt, RVec<float> &&_muon_eta, RVec<float> &&_muon_phi, RVec<float> &&_electron_pt,
-                       RVec<float> &&_electron_eta, RVec<float> &&_electron_phi, RVec<float> &&_photon_pt,
-                       RVec<float> &&_photon_eta, RVec<float> &&_photon_phi, RVec<float> &&_tau_pt, RVec<float> &&_tau_eta,
-                       RVec<float> &&_tau_phi, RVec<float> &&_bjet_pt, RVec<float> &&_bjet_eta, RVec<float> &&_bjet_phi,
-                       RVec<float> &&_jet_pt, RVec<float> &&_jet_eta, RVec<float> &&_jet_phi, RVec<float> &&_met_pt,
-                       RVec<float> &&_met_phi)
-    {
-        Muon_pt = std::move(_muon_pt);
-        Muon_eta = std::move(_muon_eta);
-        Muon_phi = std::move(_muon_phi);
-        nMuon = Muon_pt.size();
-
-        Electron_pt = std::move(_electron_pt);
-        Electron_eta = std::move(_electron_eta);
-        Electron_phi = std::move(_electron_phi);
-        nElectron = Electron_pt.size();
-
-        Photon_pt = std::move(_photon_pt);
-        Photon_eta = std::move(_photon_eta);
-        Photon_phi = std::move(_photon_phi);
-        nPhoton = Photon_pt.size();
-
-        Tau_pt = std::move(_tau_pt);
-        Tau_eta = std::move(_tau_eta);
-        Tau_phi = std::move(_tau_phi);
-        nTau = Tau_pt.size();
-
-        BJet_pt = std::move(_bjet_pt);
-        BJet_eta = std::move(_bjet_eta);
-        BJet_phi = std::move(_bjet_phi);
-        nBJet = BJet_pt.size();
-
-        Jet_pt = std::move(_jet_pt);
-        Jet_eta = std::move(_jet_eta);
-        Jet_phi = std::move(_jet_phi);
-        nJet = Jet_pt.size();
-
-        MET_pt = std::move(_met_pt);
-        MET_phi = std::move(_met_phi);
-        nMET = MET_pt.size();
     }
 
     // save storaged data
