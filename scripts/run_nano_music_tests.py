5e22a6064f69d80f1f0e7ff60719e4b3b0559c94
Felipe Silva <felipe.silva@cern.ch>
2023-04-10 20:45:39 +0200

    Update test runner

--- scripts/run_nano_music_tests.py
+++ scripts/run_nano_music_tests.py
@@ -6,24 +6,49 @@ import shlex, subprocess
 from datetime import datetime
 
 # Set environment variables
-MUSIC_BASE = os.getenv('MUSIC_BASE')
+MUSIC_BASE = os.getenv("MUSIC_BASE")
 
 task_files = glob.glob(f"{MUSIC_BASE}/configs/task_configs/*.toml")
 
 print(f"[{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}] Cleanning ...")
 
-subprocess.run(shlex.split("rm -rf Test_Ouputs_*"), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-subprocess.run(shlex.split("rm -rf test_mc_*"), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-subprocess.run(shlex.split("rm -rf test_data_*"), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+subprocess.run(
+    shlex.split("rm -rf Test_Ouputs_*"),
+    stdout=subprocess.DEVNULL,
+    stderr=subprocess.DEVNULL,
+)
+subprocess.run(
+    shlex.split("rm -rf test_mc_*"),
+    stdout=subprocess.DEVNULL,
+    stderr=subprocess.DEVNULL,
+)
+subprocess.run(
+    shlex.split("rm -rf test_data_*"),
+    stdout=subprocess.DEVNULL,
+    stderr=subprocess.DEVNULL,
+)
 
 print(f"[{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}] Starting ...")
 
 procs = []
 for task_file in task_files:
-    print(f"[{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}] Launching: nano_music --run-config {task_file}")
-    output_file = open(task_file.replace(f"{MUSIC_BASE}/configs/task_configs/", "Test_Ouputs_").replace(".toml", ".txt"), "ab")
-    procs.append(subprocess.Popen(shlex.split(f"nano_music --run-config {task_file}"), 
-    stdout=output_file, stderr=output_file))
+    if not "CRAB_" in task_file:
+        print(
+            f"[{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}] Launching: nano_music --run-config {task_file}"
+        )
+        output_file = open(
+            task_file.replace(
+                f"{MUSIC_BASE}/configs/task_configs/", "Test_Ouputs_"
+            ).replace(".toml", ".txt"),
+            "ab",
+        )
+        procs.append(
+            subprocess.Popen(
+                shlex.split(f"nano_music --run-config {task_file}"),
+                stdout=output_file,
+                stderr=output_file,
+            )
+        )
 
 
 print("")
@@ -36,4 +61,3 @@ if all(r == 0 for r in return_codes):
     print(f"[{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}] YAY !!")
 else:
     print(f"[{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}] Oops ...")
-
