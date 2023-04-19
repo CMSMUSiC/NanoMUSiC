80848d12a0218f932ebc89101b264628522238c0
Chinmay <chinmay21seth@gmail.com>
2023-04-19 14:00:43 +0200

    r Please enter the commit message for your changes. Lines starting

---
+++ NanoMUSiC/CondorScheduler/analysis.py
@@ -0,0 +1,12 @@
+
+from computation import summer
+from condor_scheduler import CondorScheduler
+
+def main():
+    condor_scheduler = CondorScheduler(output_folder = "Test_Outputs", input_folder="../CondorScheduler", prologue=["source /cvmfs/sft.cern.ch/lcg/views/LCG_102b/x86_64-centos7-gcc12-opt/setup.sh", "cd CondorScheduler","ls","pwd","hostname"])
+    for i in range(10):
+        condor_scheduler.submit_task("./computation.py", [i])
+    condor_scheduler.finalise()
+        
+if __name__=="__main__":
+    main()
