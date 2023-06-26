cdee0948ce0de7c761f2e0127a4b9709d43e85a0
Felipe Silva <felipe.silva@cern.ch>
2023-06-26 14:51:58 +0200

    Fixes the GJets generator filter.

--- NanoMUSiC/MUSiC/include/Shifts.hpp
+++ NanoMUSiC/MUSiC/include/Shifts.hpp
@@ -16,6 +16,7 @@ class Shifts
   public:
     Shifts(bool is_data)
         : m_shifts(is_data ? std::vector<std::string>{"Nominal"}
+
                            : std::vector<std::string>{"Nominal",                 //
                                                       "PU_Up",                   //
                                                       "PU_Down",                 //
