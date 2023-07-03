e0c6db5443ded3bcedb554585916c3930922f74f
Felipe Silva <felipe.silva@cern.ch>
2023-07-03 18:32:14 +0200

    Downgrade the GCC version from 12 to 11. This would make it possible to run the validation and classification code on the Linux Cluster.

--- bootstrap_music.sh
+++ bootstrap_music.sh
@@ -3,14 +3,14 @@
 # Set up the LCG environment
 # List of available software:
 # https://lcginfo.cern.ch/release_packages/x86_64-centos7-gcc11-opt/102b/
-if [[ -n ${MUSIC_ALMALINUX9+x} ]]; then
-        echo "[ LCG View ] Using x86_64-centos9-gcc12-opt ..."
-        source /cvmfs/sft.cern.ch/lcg/views/LCG_103/x86_64-centos9-gcc12-opt/setup.sh
-else
-        echo "[ LCG View ] Using x86_64-centos7-gcc12-opt ..."
-        source /cvmfs/sft.cern.ch/lcg/views/LCG_102b/x86_64-centos7-gcc12-opt/setup.sh
-fi
-echo "[ LCG View ] Done ..."86_64-centos7-gcc12-opt/setup.sh
+
+# echo "[ LCG View ] Using x86_64-centos7-gcc12-opt ..."
+# source /cvmfs/sft.cern.ch/lcg/views/LCG_102b/x86_64-centos7-gcc12-opt/setup.sh
+# echo "[ LCG View ] Done ..."
+
+echo "[ LCG View ] Using x86_64-centos7-gcc11-opt ..."
+source /cvmfs/sft.cern.ch/lcg/views/LCG_102b/x86_64-centos7-gcc11-opt/setup.sh
+echo "[ LCG View ] Done ..."
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
